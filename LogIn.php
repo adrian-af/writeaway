223d7af9d8b8fec630ac810fcd7eb5776c193d62
Luxy3000 <cilusu15@gmail.com>
2023-11-06 20:39:19 +0100

    Más estilos editados

--- LogIn.php
+++ LogIn.php
@@ -29,18 +29,28 @@
         }
     ?>
     <div class="rectangle-1">
-    <h1>Log In Form</h1>
+    <div><h1>Log In Form</h1></div>
     <div style="color: red" id="wrong"><?php echo $text;?></div>
+    <div>
     <form action="./LogInCode.php" method="GET">
-        <label for="email">Email</for>
-        <input type="email" id="email" name="email" value="<?php echo $email?>"></input><br>
-        <label for="pass">Password</for>
-        <input type="password" id="pass" name="pass"></input><br>
-        <button>Log in</button>
+        <table>
+            <tr>
+                <td><label for="email">Email</label></td>
+                <td><input class="text-input" type="email" id="email" name="email" value="<?php echo $email?>"></input></td>
+            </tr>
+            <tr>
+                <td><label for="pass">Password</label></td>
+                <td><input class="text-input" type="password" id="pass" name="pass"></input></td>
+            </tr>
+            <tr>
+                <td><button>Log in</button></td>
+            </tr>
+        </table>
     </form>
     </div>
+    </div>
     <div class="rectangle-2">
-    <p class="not-with-us-yet-">Not with us yet?</p> <button class="button-2 ">Sign IN!!</button>
+    <div class="not-with-us-yet-">Not with us yet?</div><div> <button class="button-2 ">Sign IN!!</button></div> 
     </div>
 </body>
 </html>
