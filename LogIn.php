56abd18a252e2305ef6a178e4e7572ae9bed490d
Luxy3000 <cilusu15@gmail.com>
2023-11-06 20:47:39 +0100

    ajustes

--- LogIn.php
+++ LogIn.php
@@ -29,25 +29,25 @@
         }
     ?>
     <div class="rectangle-1">
-    <div><h1>Log In Form</h1></div>
-    <div style="color: red" id="wrong"><?php echo $text;?></div>
-    <div>
-    <form action="./LogInCode.php" method="GET">
-        <table>
-            <tr>
-                <td><label for="email">Email</label></td>
-                <td><input class="text-input" type="email" id="email" name="email" value="<?php echo $email?>"></input></td>
-            </tr>
-            <tr>
-                <td><label for="pass">Password</label></td>
-                <td><input class="text-input" type="password" id="pass" name="pass"></input></td>
-            </tr>
-            <tr>
-                <td><button>Log in</button></td>
-            </tr>
-        </table>
-    </form>
-    </div>
+        <div><h2>Log In Form</h2></div>
+        <div style="color: red" id="wrong"><?php echo $text;?></div>
+        <div>
+            <form action="./LogInCode.php" method="GET">
+                <table>
+                    <tr>
+                        <td><label for="email">Email</label></td>
+                        <td><input class="text-input" type="email" id="email" name="email" value="<?php echo $email?>"></input></td>
+                    </tr>
+                    <tr>
+                        <td><label for="pass">Password</label></td>
+                        <td><input class="text-input" type="password" id="pass" name="pass"></input></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><button>Log in</button></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
     </div>
     <div class="rectangle-2">
     <div class="not-with-us-yet-">Not with us yet?</div><div> <button class="button-2 ">Sign IN!!</button></div> 
