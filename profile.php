d3878efbd57701d314378e2f7d5dc7f238975143
adri <adrian.fernandez.close@gmail.com>
2023-11-07 20:35:41 +0100

    user profile with deleting stories

--- profile.php
+++ profile.php
@@ -4,22 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+        session_start();
+        include "./dbfunctions.php";
+        $user = $_SESSION['username'];
+        $userId = $_SESSION['userId'];
+        $query = "SELECT * FROM users WHERE ID LIKE $userId";
+        $result = connectionToDB($query);
+    ?>
 </head>
 <body>
     <header>
         <div>
             <div id='pfp'>
-                <img src="" alt="">
-                <img src="" alt="">
+                <img src="<?php
+                    if($result)
+                    {
+                        $user = $result->fetch();
+                        if($user['photo'] != NULL)
+                        {
+                            $userpfp = $user['photo'];
+                        }
+                        else
+                        {
+                            $userpfp = "./Imagenes/user.png";
+                        }
+                    }
+                    echo $userpfp;
+                ?>" alt="user profile picture">
+                <img src="./Imagenes/edit.png" alt="edit button" style="width: 20px">
             </div>
         </div>
         <div>
+            <div id='about'>
+                <p>About <?php echo $user['username']; ?>:</p>
+                <p><?php echo $user['about'];?></p>
+            </div>
             <?php
-                session_start();
-                include "./dbfunctions.php";
-                $user = $_SESSION['username'];
-                $userId = $_SESSION['userId'];
-                echo $user;
+                
+                echo $user['username'];
             ?>'s stories<br>
         </div>
     </header>
@@ -29,19 +52,24 @@
             $result = connectionToDB($query);
             if($result->rowCount() > 0) //if the query returns nothing, the email is not registered
             {
-            foreach($result as $line) //takes each result of the query
-            {
-            $title = $line['title'];
-            $content = substr($line['text'], 0, 50);
-            $datetime = $line['datetime'];
-            $ID = $line['ID'];
+                foreach($result as $line) //takes each result of the query
+                {
+                    $title = $line['title'];
+                    $content = substr($line['text'], 0, 50);
+                    $mysqlDatetime = $line['datetime'];
+                    $datetime = new DateTime($mysqlDatetime);
+                    $formattedDatetime = $datetime->format('Y-m-d H:i:s');
 
-            echo "<div id='$ID'>";
-                echo "<div class='title'>$title</div>";
-                echo "<div class='content>$content</div>'";
-                echo "<div class='dateTime'>$datetime</div>";
-            echo "</div>";
-            }
+                    $ID = $line['ID'];
+
+                    echo "<div id='$ID'>";
+                        echo "<div class='title'><a href='./seeStory.php?id=$ID'>$title</a> - <a href='./editStory.php?id=$ID'>Edit</a> <a href='deleteStory.php?id=$ID'>Delete</a></div>";
+                        echo "<div class='content'>$content...</div>";
+                        echo "<div class='dateTime'>";
+                        echo $formattedDatetime; 
+                        echo "</div>";
+                    echo "</div>";
+                }
             }
             else
             {
