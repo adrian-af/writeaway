06607dba21b105d3a8ea2548e787c28bb25d7a04
Luxy3000 <cilusu15@gmail.com>
2023-11-07 20:39:55 +0100

    estilos terminados????? faltan cosas :(

--- LogIn.php
+++ LogIn.php
@@ -29,28 +29,35 @@
         }
     ?>
     <div class="rectangle-1">
-        <div><h2>Log In Form</h2></div>
-        <div style="color: red" id="wrong"><?php echo $text;?></div>
-        <div>
-            <form action="./LogInCode.php" method="GET">
-                <table>
-                    <tr>
-                        <td><label for="email">Email</label></td>
-                        <td><input class="text-input" type="email" id="email" name="email" value="<?php echo $email?>"></input></td>
-                    </tr>
-                    <tr>
-                        <td><label for="pass">Password</label></td>
-                        <td><input class="text-input" type="password" id="pass" name="pass"></input></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2"><button>Log in</button></td>
-                    </tr>
-                </table>
-            </form>
-        </div>
+        <div><h1>Log In</h1></div>
+        <form action="./LogInCode.php" method="GET">
+            <table>
+                <tr>
+                    <td colspan="2">
+                    <div style="color: red" id="wrong">
+                    <?php echo $text;?></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label class="label" for="email">Email</label></td>
+                    <td><input class="text-input" type="email" id="email" name="email" value="<?php echo $email?>"></input></td>
+                </tr>
+                <tr>
+                    <td><label class="label" for="pass">Password</label></td>
+                    <td><input class="text-input" type="password" id="pass" name="pass"></input></td>
+                </tr>
+                <tr>
+                    <td class="notes" colspan="2"><button>Log in</button></td>
+                </tr>
+            </table>
+        </form>
     </div>
     <div class="rectangle-2">
-    <div class="not-with-us-yet-">Not with us yet?</div><div> <button class="button-2 ">Sign IN!!</button></div> 
+        <div class="not-with-us-yet-">Not with us yet?</div>
+        <div>
+            <a href="SignIn.php">
+            <button class="button">Sign IN!!</button>
+        </div> 
     </div>
 </body>
 </html>
