64060c01ee80949fc5e0e12e632cc6d6c93ac418
adrian-af <adrian.fernandez.close@gmail.com>
2023-11-22 11:48:54 +0100

    fixed minor mistakes in sign in page

--- SignIn.php
+++ SignIn.php
@@ -30,10 +30,6 @@
             {
                 $text .= " The passwords do not match.";
             }
-            if($_GET['errorVerify'] == '1')
-            {
-                $text .= " There was an error verifying your email. Please try again.";
-            }
         }
     ?>
     <div class="rectangle-1">
